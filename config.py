658a15b041098bf56bcfa763e042dfce11f3c5f7
Mahesh8188 <ddbambhava8182@gmail.com>
2026-01-15 16:27:42 +0530

    Set default values for API and owner configurations
    Updated API_ID, API_HASH, OWNER_ID, and F_SUB with default values.

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 from os import environ
 
 # Telegram Account Api Id And Api Hash
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
+API_ID = int(environ.get("API_ID", "26741021"))
+API_HASH = environ.get("API_HASH", "7c5af0b88c33d2f5cce8df5d82eb2a94")
 
 # Your Main Bot Token 
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 # Owner ID For Broadcasting 
-OWNER_ID = int(environ.get("OWNER_ID", "")) # Owner Id or Admin Id
+OWNER_ID = int(environ.get("OWNER_ID", "6859451629")) # Owner Id or Admin Id
 
 # Give Your Force Subscribe Channel Id Below And Make Bot Admin With Full Right.
-F_SUB = environ.get("F_SUB", "")
+F_SUB = environ.get("F_SUB", "-1002437541681")
 
 # Mongodb Database Uri For User Data Store 
 MONGO_DB_URI = environ.get("MONGO_DB_URI", "")
